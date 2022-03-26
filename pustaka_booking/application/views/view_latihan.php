65c28fff0fadd2b673bc22f66b9fd8e0cde03361
WP2_KhoirulIqbalRamadhani <ramadhaniiqbal1999@gmail.com>
2022-03-26 13:23:11 +0700

    first commit

---
+++ pustaka_booking/application/views/view_latihan.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <tittle>Latihan 1</tittle>
+</head>
+
+<body>
+    Halo Bro.. yuk kita belajar web programming...!<br>
+    Nilai 1 = <?= $nilai1; ?>
+    Nilai 2 = <?= $nilai2; ?>
+    ini hasil dari pemodelan dengan methode penjumlahan yaitu
+    <?= $nilai1 . " + " . $nilai2 . " = " . $hasil; ?></br>
+</body>
+</html>
