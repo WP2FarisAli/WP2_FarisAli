27d6afbda03d808fd345f157c47d72daa3f7c7da
WP2FarisAli <17191317@bsi.ac.id>
2022-03-28 12:11:10 +0700

    commit on 'master'

--- pustaka_booking/application/views/view_latihan.php
+++ pustaka_booking/application/views/view_latihan.php
@@ -1,13 +1,14 @@
 <html>
+
 <head>
-    <tittle>Latihan 1</tittle>
+    <title>Latihan 1</title>
 </head>
 
 <body>
-    Halo Bro.. yuk kita belajar web programming...!<br>
+    Hallo Kawan.. Yuk kita belajar web programming...!!!!</br>
     Nilai 1 = <?= $nilai1; ?>
     Nilai 2 = <?= $nilai2; ?>
-    ini hasil dari pemodelan dengan methode penjumlahan yaitu
-    <?= $nilai1 . " + " . $nilai2 . " = " . $hasil; ?></br>
+    ini hasil dari pemodelan dengan methode penjumlahan yaitu <?= $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
 </body>
+
 </html>
