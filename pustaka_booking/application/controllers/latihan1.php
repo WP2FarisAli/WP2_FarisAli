aa84867e0522a55030770b9115979c486cb290b9
WP2FarisAli <17191317@bsi.ac.id>
2022-03-28 12:35:56 +0700

    commit on 'master'

--- pustaka_booking/application/controllers/latihan1.php
+++ pustaka_booking/application/controllers/latihan1.php
@@ -5,7 +5,7 @@ class Latihan1 extends CI_Controller
     {
         echo "<h1>Perkenalkan</h1>";
         echo "Selamat Datang.. Selamat Belajar Web Programming";
-        //$this->load->view('view_latihan1');
+        //$this->load->view('View-latihan1');
     }
     
     public function penjumlahan($n1, $n2)
@@ -14,6 +14,6 @@ class Latihan1 extends CI_Controller
         $data['nilai1'] = $n1;
         $data['nilai2'] = $n2;
         $data['hasil'] = $this->Model_latihan1->jumlah($n1, $n2);
-        $this->load->view('view_latihan1',$data);
+        $this->load->view('View-latihan1',$data);
 }
 }
